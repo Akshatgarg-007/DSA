64e288085b2ddc3db18a77b23d4d177f64e0c83b
Akshatgarg-007 <code2rizz@gmail.com>
2025-09-07 04:55:00 +0530

    Pattern

---
+++ DAY_1/left_triangle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void triangle_pattern(int n) {
+    for (int i = 0 ; i<=n; i++) {
+        for (int j= 0; j<i; j++) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    triangle_pattern(n);
+    return 0;
+}
